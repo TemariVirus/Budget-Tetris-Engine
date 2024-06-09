c2cb87a8c73c3a2284cc6185c2dbf74f58a6d3c0
TemariVirus <kiyonnxperiaz@gmail.com>
2024-06-09 16:23:03 +0800

    Upgrade Zig to 0.13

--- build.zig
+++ build.zig
@@ -14,7 +14,12 @@ pub fn build(b: *Build) void {
 
     // Expose the library root
     _ = b.addModule("engine", .{
-        .root_source_file = .{ .path = "src/root.zig" },
+        .root_source_file = .{
+            .src_path = .{
+                .owner = b,
+                .sub_path = "src/root.zig",
+            },
+        },
         .imports = &.{
             .{ .name = "nterm", .module = nterm_module },
         },
@@ -25,7 +30,12 @@ pub fn build(b: *Build) void {
 
 fn buildTests(b: *Build) void {
     const lib_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/root.zig" },
+        .root_source_file = .{
+            .src_path = .{
+                .owner = b,
+                .sub_path = "src/root.zig",
+            },
+        },
     });
     const run_lib_tests = b.addRunArtifact(lib_tests);
     const test_step = b.step("test", "Run library tests");
